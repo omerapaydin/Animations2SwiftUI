d318dd93eef1c5f1e0f1516ecdc81519f58daaa4
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-18 00:43:39 +0300

    second

--- Animations2SwiftUI/ContentView.swift
+++ Animations2SwiftUI/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isAnimating = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .fill(isAnimating ? Color.red : Color.blue)
+                .frame(width: isAnimating ? 150 : 250, height: isAnimating ? 150 : 250)
+                .animation(.easeInOut(duration: 1),value: isAnimating)
+            
+            
+            Button("Animate") {
+                isAnimating.toggle()
+            }.padding()
         }
         .padding()
     }
